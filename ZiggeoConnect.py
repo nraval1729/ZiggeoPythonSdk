e01bc7ab73ca2bfbd5ee946fde7b17781e586081
Nisarg Raval <nisargraval95@gmail.com>
2019-10-31 10:24:34 +0530

    Use py2/3 imports in ZiggeoConnect

--- ZiggeoConnect.py
+++ ZiggeoConnect.py
@@ -5,14 +5,12 @@ from past.builtins import basestring
 from builtins import object
 import base64, json, ntpath
 
-try:
-    #For Python 3.0 and later
-    from urllib import request as urllib2
-    from urllib import parse as urllib
-    basestring = str
-except ImportError:
-    #For Python 2's urllib2
-    import urllib.request, urllib.error, urllib.parse, urllib.request, urllib.parse, urllib.error
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
 
 from MultiPartForm import MultiPartForm
 
@@ -25,34 +23,34 @@ class ZiggeoConnect(object):
     def request(self, method, path, data = None, file = None, timeout=60):
         path = path.encode("ascii", "ignore")
         if (method == "GET" and data != None):
-            path = path.decode('ascii', 'ignore') + "?" + urllib.parse.urlencode(data)
+            path = path.decode('ascii', 'ignore') + "?" + urlencode(data)
         if (method != "GET" and method != "POST"):
             path = path.decode('ascii', 'ignore') + "?_method=" + method
 
         if not isinstance(path, basestring):
             path = path.decode("ascii", "ignore")
 
-        request = urllib.request.Request(self.__baseuri + path)
+        request = Request(self.__baseuri + path)
 
         base64string = base64.encodestring(('%s:%s' % (self.__application.token, self.__application.private_key)).encode()).decode().replace('\n', '')
 
         request.add_header("Authorization", "Basic %s" % base64string)
         if (method == "GET"):
-            result = urllib.request.urlopen(request, None, timeout)
+            result = urlopen(request, None, timeout)
         else:
             if (data == None):
                 data = {}
             if (file == None):
-                data = urllib.parse.urlencode(data)
+                data = urlencode(data)
                 binary_data = data.encode("ascii")
-                result = urllib.request.urlopen(request, binary_data, timeout)
+                result = urlopen(request, binary_data, timeout)
             else:
                 form_file = [('file', ntpath.basename(file), open(file, "rb"))]
                 content_type, body = MultiPartForm().encode(data, form_file)
 
                 request.add_header('Content-type', content_type)
                 request.add_header('Content-length', len(body))
-                result = urllib.request.urlopen(request, body, timeout)
+                result = urlopen(request, body, timeout)
 
         try:
             accept_ranges = result.getheader('Accept-Ranges')

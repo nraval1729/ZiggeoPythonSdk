e846d6a89e808e402665d50dfee2c9e0eb71109a
Nisarg Raval <nisargraval95@gmail.com>
2019-10-31 11:40:34 +0530

    First cut of porting ZiggeoConnect/request() from urllib -> requests

--- ZiggeoConnect.py
+++ ZiggeoConnect.py
@@ -22,95 +22,70 @@ class ZiggeoConnect(object):
         self.__application = application
         self.__baseuri = baseuri
 
-    def request_new(self, method, path, data = None, file = None, timeout=60):
+    def request(self, method, path, data = None, file = None, timeout=60):
         base64string = base64.encodestring(('%s:%s' % (self.__application.token, self.__application.private_key)).encode()).decode().replace('\n', '')
         headers = {"Authorization": "Basic %s" % base64string}
+
         if method == "GET":
-            result = requests.get(self.__baseuri+path, params=data, headers=headers)
-            return result.json()
-        # path = path.encode("ascii", "ignore")
-        # if (method == "GET" and data != None):
-        #     path = path.decode('ascii', 'ignore') + "?" + urlencode(data)
-        # if (method != "GET" and method != "POST"):
-        #     path = path.decode('ascii', 'ignore') + "?_method=" + method
-        #
-        # if not isinstance(path, basestring):
-        #     path = path.decode("ascii", "ignore")
-        #
-        # request = Request(self.__baseuri + path)
-        #
-        #
-        # request.add_header("Authorization", "Basic %s" % base64string)
-        # if (method == "GET"):
-        #     result = urlopen(request, None, timeout)
-        # else:
-        #     if (data == None):
-        #         data = {}
-        #     if (file == None):
-        #         data = urlencode(data)
-        #         binary_data = data.encode("ascii")
-        #         result = urlopen(request, binary_data, timeout)
-        #     else:
-        #         form_file = [('file', ntpath.basename(file), open(file, "rb"))]
-        #         content_type, body = MultiPartForm().encode(data, form_file)
-        #
-        #         request.add_header('Content-type', content_type)
-        #         request.add_header('Content-length', len(body))
-        #         result = urlopen(request, body, timeout)
-        #
-        # try:
-        #     accept_ranges = result.getheader('Accept-Ranges')
-        #     if (accept_ranges == 'bytes'):
-        #         return result.read()
-        #     else:
-        #         return result.read().decode('ascii')
-        # except AttributeError as e:
-        #     return result.read()
-
-
-    def request(self, method, path, data = None, file = None, timeout=60):
-        path = path.encode("ascii", "ignore")
-        if (method == "GET" and data != None):
-            path = path.decode('ascii', 'ignore') + "?" + urlencode(data)
-        if (method != "GET" and method != "POST"):
-            path = path.decode('ascii', 'ignore') + "?_method=" + method
-
-        if not isinstance(path, basestring):
-            path = path.decode("ascii", "ignore")
-
-        request = Request(self.__baseuri + path)
-
-        base64string = base64.encodestring(('%s:%s' % (self.__application.token, self.__application.private_key)).encode()).decode().replace('\n', '')
-
-        request.add_header("Authorization", "Basic %s" % base64string)
-        if (method == "GET"):
-            result = urlopen(request, None, timeout)
+            result = requests.get(self.__baseuri+path, params=data, headers=headers, timeout=timeout)
         else:
-            if (data == None):
-                data = {}
-            if (file == None):
-                data = urlencode(data)
-                binary_data = data.encode("ascii")
-                result = urlopen(request, binary_data, timeout)
+            data = {} if data is None else data
+            if file is None:
+                result = requests.request(method=method, url=self.__baseuri + path, data=data, headers=headers, timeout=timeout)
             else:
                 form_file = [('file', ntpath.basename(file), open(file, "rb"))]
                 content_type, body = MultiPartForm().encode(data, form_file)
 
-                request.add_header('Content-type', content_type)
-                request.add_header('Content-length', len(body))
-                result = urlopen(request, body, timeout)
+                headers.update({'Content-type': content_type})
+                headers.update({'Content-length': len(body)})
 
-        try:
-            accept_ranges = result.getheader('Accept-Ranges')
-            if (accept_ranges == 'bytes'):
-                return result.read()
-            else:
-                return result.read().decode('ascii')
-        except AttributeError as e:
-            return result.read()
+                result = requests.request(method=method, url=self.__baseuri + path, data=body, headers=headers, timeout=timeout)
+
+        return result.json()
+
+    # def request(self, method, path, data = None, file = None, timeout=60):
+    #     path = path.encode("ascii", "ignore")
+    #     if (method == "GET" and data != None):
+    #         path = path.decode('ascii', 'ignore') + "?" + urlencode(data)
+    #     if (method != "GET" and method != "POST"):
+    #         path = path.decode('ascii', 'ignore') + "?_method=" + method
+    #
+    #     if not isinstance(path, basestring):
+    #         path = path.decode("ascii", "ignore")
+    #
+    #     request = Request(self.__baseuri + path)
+    #
+    #     base64string = base64.encodestring(('%s:%s' % (self.__application.token, self.__application.private_key)).encode()).decode().replace('\n', '')
+    #
+    #     request.add_header("Authorization", "Basic %s" % base64string)
+    #     if (method == "GET"):
+    #         result = urlopen(request, None, timeout)
+    #     else:
+    #         if (data == None):
+    #             data = {}
+    #         if (file == None):
+    #             data = urlencode(data)
+    #             binary_data = data.encode("ascii")
+    #             result = urlopen(request, binary_data, timeout)
+    #         else:
+    #             form_file = [('file', ntpath.basename(file), open(file, "rb"))]
+    #             content_type, body = MultiPartForm().encode(data, form_file)
+    #
+    #             request.add_header('Content-type', content_type)
+    #             request.add_header('Content-length', len(body))
+    #             result = urlopen(request, body, timeout)
+    #
+    #     try:
+    #         accept_ranges = result.getheader('Accept-Ranges')
+    #         if (accept_ranges == 'bytes'):
+    #             return result.read()
+    #         else:
+    #             return result.read().decode('ascii')
+    #     except AttributeError as e:
+    #         return result.read()
 
     def requestJSON(self, method, path, data = None, file = None):
-        return json.loads(self.request_new(method, path, data, file))
+        return json.loads(self.request(method, path, data, file))
 
     def get(self, path, data = None, file = None):
         return self.request("GET", path, data, file)
